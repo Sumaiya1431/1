19b59a71c65b7c0013914d4709c036ee154cd309
Sumaiya1431 <sumaiya.garag@gmail.com>
2016-09-26 22:13:54 +0530

    Add files via upload

---
+++ CricketPlayerDemo.java
@@ -0,0 +1,22 @@
+package in.ac.kletech.CricketPlayer;
+
+public class CricketPlayerDemo 
+{
+
+	public static void main(String[] args)
+	{	
+		CricketPlayer[] c=new CricketPlayer[5];
+	    CricketPlayer m= new CricketPlayer();
+		c=m.createPlayers();
+		System.out.println(" before sorting \n");
+		
+		m.printPlayer();
+		
+		System.out.println("after sorting \n");
+		
+		m.printPlayer(c);
+		
+	}
+	
+}
+
